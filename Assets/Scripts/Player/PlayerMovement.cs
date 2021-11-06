d939e160817f104f33e4b3df5b3427d5ec4da696
JoniHelen <djohe015@edu.xamk.fi>
2021-11-06 23:29:18 +0200

    Update PlayerMovement.cs

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,7 +7,7 @@ using UniRx;
 public class PlayerMovement : MonoBehaviour
 {
 
-    [SerializeField] VectorThree_SO startPos;
+    Vector3 startPos;
     [SerializeField] float jumpForce = 5;
     [SerializeField] float speed = 5;
     [SerializeField] float gravity = -9.81f;
@@ -21,11 +21,10 @@ public class PlayerMovement : MonoBehaviour
     CharacterController controller;
     Vector3 direction = Vector3.zero;
     bool isGrounded;
-    [SerializeField] SpriteRenderer renderer;
     // Start is called before the first frame update
     void Start()
     {
-        startPos.Set(transform.position);
+        startPos = transform.position;
         controller = GetComponent<CharacterController>();
     }
 
@@ -71,14 +70,6 @@ public class PlayerMovement : MonoBehaviour
     public void OnMove(InputAction.CallbackContext ctx)
     {
         direction.x = ctx.ReadValue<float>() * speed;
-        if(direction.x > 0 && renderer.flipX)
-        {
-            renderer.flipX = false;
-        }
-        if(direction.x < 0 && !renderer.flipX)
-        {
-            renderer.flipX = true;
-        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -87,7 +78,7 @@ public class PlayerMovement : MonoBehaviour
         {
             direction = Vector3.zero;
             controller.enabled = false;
-            transform.position =  startPos.Get() + Vector3.up;
+            transform.position = startPos + Vector3.up;
             controller.enabled = true;
         }
     }
