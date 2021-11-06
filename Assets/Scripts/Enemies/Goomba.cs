3655cd824f318b3ce4367556855003ac61a63bbd
Drila008 <91053320+Drila008@users.noreply.github.com>
2021-11-06 20:28:41 +0200

    Mob color behavior
    Color behavior for blue, red, green and white

--- Assets/Scripts/Enemies/Goomba.cs
+++ Assets/Scripts/Enemies/Goomba.cs
@@ -2,23 +2,93 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Goomba : MonoBehaviour
+public class Goomba : ColorBehaviour
 {
+    public float jumpPower = 0.5f;
     public float speed;
     public Rigidbody goombaRB;
+    float time = 0f;
+    private bool isJumping = false;
+    public Vector3 startPos;
+
+    bool hasTriggered = false;
+
+    public GameObject player;
 
     [SerializeField] GameObject target;
     // Start is called before the first frame update
     void Start()
     {
         goombaRB = GetComponent<Rigidbody>();
+        startPos = transform.position;
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        goombaRB.velocity = new Vector2(speed, goombaRB.velocity.y);
+        //goombaRB.velocity = new Vector2(speed, goombaRB.velocity.y);
 
         
     }
+
+    protected override void Behaviours()
+    {
+        //base.Behaviours();
+        switch (_mySpec)
+        {
+            case ColorSpectrum.ColorSpec.Green:
+                if (!hasTriggered)
+                {
+                    hasTriggered = true;
+                    startPos = transform.position;
+                }                
+     
+                Physics.IgnoreCollision(GetComponent<Collider>(), player.GetComponent<Collider>(), false);
+                time += Time.deltaTime;
+                transform.position = startPos + Vector3.right * Mathf.Sin(time * 1.5f) * 2;
+                break;
+
+            case ColorSpectrum.ColorSpec.Red:
+                Physics.IgnoreCollision(GetComponent<Collider>(), player.GetComponent<Collider>(), false);
+                hasTriggered = false;
+                goombaRB.useGravity = true;
+
+                if (goombaRB.velocity.magnitude > 0)
+                {
+                    isJumping = true;
+                }
+                else
+                {
+                    isJumping = false;
+                }
+                if(!isJumping)
+                {
+                    goombaRB.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+                }
+                time = 0f;
+                break;
+
+            case ColorSpectrum.ColorSpec.Blue:
+                hasTriggered = false;
+                Physics.IgnoreCollision(GetComponent<Collider>(), player.GetComponent<Collider>(), false);
+                goombaRB.velocity = new Vector3(0, 0, 0);
+                goombaRB.useGravity = false;
+                transform.position = transform.position;
+                time = 0f;
+                break;
+
+            case ColorSpectrum.ColorSpec.White:
+                hasTriggered = false;
+                goombaRB.useGravity = true;
+                Physics.IgnoreCollision(GetComponent<Collider>(), player.GetComponent<Collider>(), true);
+                time = 0f;
+                break;
+          
+            default:
+                break;
+        }
+
+    }
+
 }
