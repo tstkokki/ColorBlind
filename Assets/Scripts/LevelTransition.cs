12ef8f0bfa56cc4ed843450ee00f2b5a321d7e4f
tstkokki <60537602+tstkokki@users.noreply.github.com>
2021-11-07 00:08:29 +0200

    level handlers folder

--- Assets/Scripts/LevelTransition.cs
+++ Assets/Scripts/LevelTransition.cs
@@ -7,7 +7,7 @@ public class LevelTransition : MonoBehaviour
     // reference to next level prefab
     [SerializeField] GameObject nextLevel;
     //reference to next safe point
-
+    [SerializeField] ParticleSystem bigSmoke;
     Collider collider;
     // Start is called before the first frame update
     void Start()
@@ -20,7 +20,7 @@ public class LevelTransition : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             collider.enabled = false;
-            
+            Instantiate(bigSmoke, transform.position, bigSmoke.transform.rotation);
         }
     }
 }
