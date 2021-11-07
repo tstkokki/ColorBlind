93794308b42ac36d0b85a909488c65b4349ddb2a
JoniHelen <djohe015@edu.xamk.fi>
2021-11-07 14:27:59 +0200

    text

--- Assets/Scripts/LevelTransition.cs
+++ Assets/Scripts/LevelTransition.cs
@@ -7,6 +7,7 @@ public class LevelTransition : MonoBehaviour
     // reference to next level prefab
     [SerializeField] GameObject nextLevel;
     [SerializeField] GameEvent movePlayer;
+    [SerializeField] GameEvent levelTransition;
     //reference to next safe point
     [SerializeField] ParticleSystem bigSmoke;
     Collider collider;
@@ -25,6 +26,7 @@ public class LevelTransition : MonoBehaviour
 
             Instantiate(bigSmoke, transform.position, bigSmoke.transform.rotation);
             movePlayer.Raise();
+            levelTransition.Raise();
         }
     }
 }
